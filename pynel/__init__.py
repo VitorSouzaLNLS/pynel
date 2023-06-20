b02aa184efa3abf95b3a715e86955932fe498483
VitorSouzaLNLS <vitor.souza@lnls.br>
2023-06-20 09:46:54 -0300

    change_iterations_to_fitting

--- pynel/__init__.py
+++ pynel/__init__.py
@@ -1,10 +1,10 @@
 """Pynel package."""
 
-from . import data_test_fix
+from . import std_si_data
 from . import misc_functions
 from .buttons import Button
 from .base import Base
-from . import iterations
+from . import fitting
 
 #from . import std_si_data
 #from . import last_complete_pynel as completePynel
@@ -13,4 +13,4 @@ import os as _os
 with open(_os.path.join(__path__[0], 'VERSION'), 'r') as _f:
     __version__ = _f.read().strip()
 
-__all__ = ['Base', 'Button', 'misc_functions', 'iterations', 'data_test_fix']
+__all__ = ['Base', 'Button', 'misc_functions', 'fitting', 'std_si_data']
